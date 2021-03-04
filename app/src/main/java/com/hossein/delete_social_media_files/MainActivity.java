79b6a2cfe0e42a62d037044e295d02c6ed4513da
cpt-hossein <h04524223946>
2021-03-04 14:00:42 +0330

    init files

--- app/src/main/java/com/hossein/delete_social_media_files/MainActivity.java
+++ app/src/main/java/com/hossein/delete_social_media_files/MainActivity.java
@@ -1,14 +1,38 @@
 package com.hossein.delete_social_media_files;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.io.File;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+
+public class MainActivity extends AppCompatActivity {
+    File dir = getFilesDir();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        deleteDirectory(dir);
+
+}
+
+    static public boolean deleteDirectory(File path) {
+        if( path.exists() ) {
+            File[] files = path.listFiles();
+            if (files == null) {
+                return true;
+            }
+            for(int i=0; i<files.length; i++) {
+                if(files[i].isDirectory()) {
+                    deleteDirectory(files[i]);
+                } else {
+                    files[i].delete();
+                }
+            }
+        }
+        return( path.delete() );
     }
+
 }
