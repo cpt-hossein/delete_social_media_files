835e9ef0170831f7ab7575e0f2e8d1c82a4efa84
cpt-hossein <h04524223946>
2021-03-04 13:16:16 +0330

    init files

---
+++ app/src/main/java/com/hossein/delete_social_media_files/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hossein.delete_social_media_files;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
